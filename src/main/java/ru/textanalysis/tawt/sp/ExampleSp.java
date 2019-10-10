27df265238a4ded29d2a3f93274ae7e058a57eeb
jalexpr <alex.porechny@mail.ru>
2019-10-10 20:54:58 +0300

    1.1.4 syntactic-parser

--- src/main/java/ru/textanalysis/tawt/sp/ExampleSp.java
+++ src/main/java/ru/textanalysis/tawt/sp/ExampleSp.java
@@ -3,12 +3,17 @@ package ru.textanalysis.tawt.sp;
 import ru.textanalysis.tawt.ms.internal.sp.BearingPhraseSP;
 import ru.textanalysis.tawt.sp.api.SyntaxParser;
 
+import java.util.List;
+
 public class ExampleSp {
     public static void main(String[] args) {
+
+//        BearingPhraseSP phrase1 = sp.getTreeSentence("Солнце село за село.");
+//        System.out.println(phrase1);
+//
         SyntaxParser sp = new SyntaxParser();
         sp.init();
-
-        BearingPhraseSP phrase = sp.getTreeSentence("Солнце село за село.");
-        System.out.println(phrase);
+        List<BearingPhraseSP> phrase = sp.getTreeSentence("стало ясно, что будет с российской валютой");
+        phrase.forEach(System.out::println);
     }
 }
--- src/main/java/ru/textanalysis/tawt/sp/api/SyntaxParser.java
+++ src/main/java/ru/textanalysis/tawt/sp/api/SyntaxParser.java
@@ -3,42 +3,76 @@ package ru.textanalysis.tawt.sp.api;
 import ru.textanalysis.tawt.awf.AWF;
 import ru.textanalysis.tawt.gama.main.Gama;
 import ru.textanalysis.tawt.ms.internal.sp.BearingPhraseSP;
-import ru.textanalysis.tawt.ms.storage.BearingPhraseList;
-import ru.textanalysis.tawt.ms.storage.WordList;
-import ru.textanalysis.tawt.ms.storage.ref.RefBearingPhraseList;
+import ru.textanalysis.tawt.ms.internal.sp.OmoFormSP;
+import ru.textanalysis.tawt.ms.internal.sp.WordSP;
+import ru.textanalysis.tawt.rfc.RulesForCompatibility;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static ru.textanalysis.tawt.rfc.Utils.installCompatibility;
 
 public class SyntaxParser {
-    //todo
     private Gama gama = new Gama();
     private AWF awf = new AWF();
+    private RulesForCompatibility rules = new RulesForCompatibility();
 
     public void init() {
         gama.init();
         awf.init();
     }
 
-    public BearingPhraseSP getTreeSentence(String text) {
-        RefBearingPhraseList bearingPhraseList = gama.getMorphSentence(text);
-        BearingPhraseSP bearingPhrase = new BearingPhraseSP(bearingPhraseList.get(0)); //todo пока что работа с первым опорным оборотом
-        awf.applyAwfForBearingPhrase(bearingPhrase);
-        bearingPhrase.searchMainOmoForm();
-        return bearingPhrase;
+    public List<BearingPhraseSP> getTreeSentence(String text) {
+        List<BearingPhraseSP> bearingPhraseList = gama.getMorphSentence(text).stream().map(BearingPhraseSP::new).collect(Collectors.toList());
+        bearingPhraseList.forEach(awf::applyAwfForBearingPhrase);
+        bearingPhraseList.forEach(this::applyCompatibility);
+        bearingPhraseList.forEach(BearingPhraseSP::searchMainOmoForm);
+        return bearingPhraseList;
     }
 
-    private void settingLinksBearingPhrases(BearingPhraseList bearingPhrases) {
-        settingLinksWithinBearingPhrases(bearingPhrases);
-        settingLinksMainWordsBearingPhrases(bearingPhrases);
+    private void applyCompatibility(BearingPhraseSP bearingPhraseSP) {
+        bearingPhraseSP.applyConsumer(word -> {
+            establishCompatibility(word);
+            attachmentToBearingWord(word);
+            //todo
+        });
     }
 
-    private void settingLinksWithinBearingPhrases(BearingPhraseList bearingPhrases) {
-        bearingPhrases.forEach(this::settingLinksWithinOneBearingPhrase);
+    private void establishCompatibility(List<WordSP> words) {
+        for (int i = words.size() - 1; -1 < i; i--) {
+            WordSP word = words.get(i);
+            for (int j = words.size() - 1; i < j; j--) {
+                if (rules.establishRelation(j - i, word, words.get(j))) {
+                    //todo log;
+                }
+            }
+        }
     }
 
-    private void settingLinksWithinOneBearingPhrase(WordList words) {
-        //todo:связывание
-    }
-
-    private void settingLinksMainWordsBearingPhrases(BearingPhraseList bearingPhrases) {
-
+    //todo добавить разеление на опорные и неопорные слова (делать двумя иттерациями)
+    private void attachmentToBearingWord(List<WordSP> words) {
+        for (int i = words.size() - 1; 0 < i; i--) {
+            WordSP wordD = words.get(i);
+            if (!wordD.haveMain()) {
+                for (int j = i - 1; -1 < j; j--) {
+                    WordSP wordM = words.get(j);
+                    if (wordM.haveContainsBearingForm()) {
+                        OmoFormSP omoFormM = wordM.getByFilter(OmoFormSP::haveBearingForm).get(0);
+                        //todo добавить возможность привязывать не первый
+                        OmoFormSP omoFormD;
+                        List<OmoFormSP> omoFormsD = wordD.getByFilter(OmoFormSP::haveRelation);
+                        if (omoFormsD.isEmpty()) {
+                            omoFormD = wordD.getByFilter(omoFormSP -> true).get(0);
+                        } else {
+                            omoFormD = omoFormsD.get(0);
+                        }
+                        installCompatibility(wordM, wordD, omoFormM, omoFormD);
+                        wordM.cleanNotRelation();
+                        wordD.cleanNotRelation();
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
